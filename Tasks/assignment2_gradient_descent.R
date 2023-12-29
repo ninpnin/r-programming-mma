03b63875cabdeeab9c4dc87dcd4a21ab8bbc38a5
ninpnin <vainoyrjanainen@icloud.com>
2023-12-29 12:05:36 +0200

    feat: gd

--- Tasks/assignment2_gradient_descent.R
+++ Tasks/assignment2_gradient_descent.R
@@ -9,6 +9,19 @@ test_that("gradient_descent()", {
               info = "Error: gradient_descent() is missing")
   checkmate::expect_function(gradient_descent, args = c("X", "y", "epochs", "stepsize"), 
                              info = "Incorrect function arguments.")
+  data <- tibble(a=1:3, b = 1:3 * 2 -3, c = c(1,-1,0))
+  X <- data[c("a", "b")]
+  y <- c(0,0,1)
+  X <- as.matrix(X)
+  beta <- c(1.1, 0.2)
+  beta <- as.matrix(beta)
+  beta0 <- -1.3
   
+  gd_result <- gradient_descent(X, y, 100, 0.1)
+  expect_equivalent(gd_result$beta,
+                    c(0.7247067, 1.6009520), 
+                    tol= 0.01, 
+                    info = "Error: Incorrect result.")
+
   
 })
