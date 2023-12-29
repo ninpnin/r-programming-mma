5be4703da0b8e9dfabd618df9a553a90d4355d4a
ninpnin <vainoyrjanainen@icloud.com>
2023-12-29 11:54:03 +0200

    fix: z

--- Tasks/assignment2_z.R
+++ Tasks/assignment2_z.R
@@ -13,7 +13,7 @@ test_that("z()", {
   X <- matrix(c(1,-1,-1,1), nrow = 2, ncol = 2)
   beta <- c(0.5, 0.75)
   beta0 <- 3
-  expect_equivalent(z(data, "c", c("a", "b"), 10.0), 
+  expect_equivalent(z(X, beta, beta0), 
                     c(2.75, 3.25), 
                     tol= 0.01, 
                     info = "Error: Incorrect result.")
