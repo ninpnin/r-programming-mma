7c1ab4d641e07844f1054fb65971607b3c0953d4
ninpnin <vainoyrjanainen@icloud.com>
2023-12-29 11:52:04 +0200

    feat: gd

--- Tasks/assignment2_gradient_descent.R
+++ Tasks/assignment2_gradient_descent.R
@@ -1,19 +1,14 @@
-context("ridge()")
+context("gradient_descent()")
 
-test_that("ridge()", {  
+test_that("gradient_descent()", {  
   
   #-----------------------------------------------------------------------------------
   # test for log_importance_weights()
   #-----------------------------------------------------------------------------------
-  expect_true(exists("ridge"),
-              info = "Error: ridge() is missing")
-  checkmate::expect_function(ridge, args = c("data", "dep", "indep", "lambda"), 
+  expect_true(exists("gradient_descent"),
+              info = "Error: gradient_descent() is missing")
+  checkmate::expect_function(gradient_descent, args = c("X", "y", "epochs", "stepsize"), 
                              info = "Incorrect function arguments.")
   
-  data <- tibble(a=1:3, b = 1:3 * 2 -3, c = c(1,-1,0))
-  expect_equivalent(ridge(data, "c", c("a", "b"), 10.0), 
-                    c(0.02586207, -0.00862069, -0.09482759), 
-                    tol= 0.01, 
-                    info = "Error: Incorrect result.")
   
 })
